5bbeb045f2258f1851121dfe0eb5bf1856631031
Arash <161996381+ArashAfkandeh@users.noreply.github.com>
2025-10-01 22:26:08 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+{
+    "api_id": "YOUR_API_ID",
+    "api_hash": "YOUR_API_HASH",
+    "bot_token": "YOUR_BOT_TOKEN",
+    "file_max_age_hours": 24,
+    "your_domain": "YOUR_DOMAIN",
+    "download_path": "/dl",
+    "allowed_chat_ids": [
+        123456789,
+        987654321
+    ],
+    "proxy": {
+        "scheme": "socks5",
+        "server": "PROXY_SERVER",
+        "port": PROXY_PORT,
+        "user": "USERNAME",
+        "pass": "PASSWORD"
+    }
+}
